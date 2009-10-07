14d71bac20b7ac4a758aae6f84658d6ba9b1b927
Aaron Stone <aaron@serendipity.cx>
2009-10-07 10:15:50 -0700

    Improve options handling. Add dump mode. Add IP templating.

--- lib/slicehost-dns.rb
+++ lib/slicehost-dns.rb
@@ -5,36 +5,67 @@ require 'activeresource'
 require 'yaml'
 require 'logger'
 
+# Override the logger output to show just the messgae
 class Logger
   class Formatter
     def call(severity, time, progname, msg)
       "#{msg}\n"
+      # FIXME: exit on fatal messages
     end
   end
 end
 
-# yes globals are evil but hey, i welcome patches. 
-# and, a riddle: what are singletons except globals reduced to a pattern?
+# Global logger and verbosity level
 $LOG = Logger.new($stdout)
-
-def error(message) 
-  $LOG.fatal(message)
-  exit 
+if ARGV.delete('--verbose')
+  $LOG.level = Logger::DEBUG
+else
+  $LOG.level = Logger::INFO
 end
 
+# Example yaml
 if ARGV.include? '--example'
   example = File.dirname(__FILE__) + '/example.yml'
-  error open(example).read 
+  puts open(example).read 
+  exit
 end
 
-error "Usage: slicehost-dns (--dump --dry) [config.yml]" if ARGV.empty?
+# Help text
+if ARGV.empty? or ARGV.delete('--help')
+  $LOG.fatal("Usage: slicehost-dns (--dump --dry --verbose --help) [config.yml]")
+  exit
+end
 
 dump = ARGV.delete('--dump')
 dry_run = ARGV.delete('--dry')
 filename = ARGV.shift
-config = YAML.load_file(filename)
+config = {}
+templates = {}
 
-API = ENV['SLICEHOST_API_KEY'] || config.delete('api')
+# In Dump mode, if there's a filename and it exists, rewrite it. If there's a
+# filename and it doesn't exist, dump to it. If there's no filename, dump to
+# stdout.
+
+if filename and FileTest.exists?(filename)
+  config = YAML.load_file(filename)
+  $LOG.debug(YAML.dump(config))
+  # Drop the templates section, since we're using it just to generate anchors
+  # and the YAML parser does all the work.
+  templates = config.delete('templates')
+elsif dump and not filename
+  filename = "-"
+elsif not dump
+  raise ArgumentError, "no filename provided"
+end
+
+# Prefer the api key from the configuration file
+if config.has_key?('api')
+  API = config.delete('api')
+  $LOG.info("Using API key from YAML file: #{API}") if not API.nil?
+else
+  API = ENV['SLICEHOST_API_KEY']
+  $LOG.info("Using API key from environment: #{API}") if not API.nil?
+end
 raise ArgumentError, "no API key found" if API.nil?
 
 $:.unshift File.dirname(__FILE__)
@@ -51,18 +82,18 @@ records = Record.find :all
 if dry_run
   class Zone
     def save
-      $LOG.info("~> dry saving zone #{origin}")
+      $LOG.debug("~> dry saving zone #{origin}")
     end
     def destroy
-      $LOG.info("~> dry destroying zone #{origin}")
+      $LOG.debug("~> dry destroying zone #{origin}")
     end
   end
   class Record
     def save
-      $LOG.info("~> dry saving record #{kind} #{name}: #{data}")
+      $LOG.debug("~> dry saving record #{kind} #{name}: #{data}")
     end
     def destroy
-      $LOG.info("~> dry destroying record #{kind} #{name}: #{data}")
+      $LOG.debug("~> dry destroying record #{kind} #{name}: #{data}")
     end
   end
 end
@@ -126,7 +157,11 @@ if dump
     output[ip][zone.domain] = domain
   end
   
-  File.open(filename, 'w') {|f| YAML.dump(output, f) }
+  if filename == "-"
+    YAML.dump(output, $stdout)
+  else
+    File.open(filename, 'w') {|f| YAML.dump(output, f) }
+  end
 else
   config.each_pair do |ip, domains|
     unless ip.match(/(\d+\.){3}\d+/)
@@ -134,12 +169,30 @@ else
       next
     end
     domains.each_pair do |domain, desired_records|
-      $LOG.info "\ndomain #{domain}"
+      using_template = false
+      if desired_records.nil?
+        if templates.has_key?(ip)
+          # There's no deep copy in Ruby, so we do this horrific thing
+          desired_records = Marshal::load(Marshal.dump(templates[ip]))
+          using_template = true
+        else
+          $LOG.warn("\ndomain #{domain}: No records, skipping ")
+          next
+        end
+      end
+      $LOG.info "\ndomain #{domain}:" unless using_template
+      $LOG.info "\ndomain #{domain} using template for #{ip}:" if using_template
       zone = zones.detect {|z| z.domain == domain } || Zone.create_for_domain(domain)
       records_for_domain, records = records.partition {|r| r.zone_id == zone.id }
+      # TODO: Separate out the reconciler from the processor, so that we
+      # can do something nice here like saying, "No changes for this domain".
       Reconciler.new(zone, ip, records_for_domain, desired_records).process!
       zones.delete(zone)
     end
-    puts "\n\ni'd delete #{zones.map{|z| z.domain}.join(', ')} now but you'd hate me."
+    leftovers = zones.map{|z| z.domain}.join(', ') unless zones.empty?
+    puts "\n\ni'd delete #{leftovers} now but you'd hate me." if leftovers
+    # TODO: let's enable deletes with this syntax in the YAML file:
+    # ip:
+    #   domain: { delete: 1 }
   end
 end
