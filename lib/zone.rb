fa84cfa1498ef29b7ec184a90187fd728e8c9a2d
Aaron Stone <aaron@serendipity.cx>
2009-10-29 14:33:09 -0700

    Method to look up zones by name.

--- lib/zone.rb
+++ lib/zone.rb
@@ -11,8 +11,12 @@ class Zone < ActiveResource::Base
     zone
   end
   
+  def self.find_by_domain(domain)
+    Zone.find(:all, :params => { :origin => domain })
+  end
+  
   def new_record(attrs={})
     Record.new(attrs.merge({:zone_id => id}))
   end
-  
+
 end
